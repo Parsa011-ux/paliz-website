510354052eecff3b152c1c5a59d85fd3ff2d4fbb
Parsa011-ux <parsaamynzadh@gmail.com>
2026-08-09 18:31:11 +0330

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -75,7 +75,9 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="پالیز نیوز" />
-        <link rel="apple-touch-icon" href="/logo.png" />
+       <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </head>
       <body className={`${vazirmatn.className} antialiased`}>
         <ProgressBar />
